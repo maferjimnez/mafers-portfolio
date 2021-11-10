d4bac908169efb296c49ce6547e59399df57d3d8
Mafer <jmmariafernanda15@gmail.com>
2021-11-10 17:11:46 -0300

    Adds media query for above 1200

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,31 +1,32 @@
 const theme = {
-    colors:{
-        primaryText: '#404040',
-        primaryBackground: '#f3f1f5',
-        secondaryBackground: '#eff8ff',
-        gifosBackground: '#a08bcc',
-        marvelBackground: '#f58e8e',
-        rewardsBackground: '#9edfee',
-        reservaBackground: '#d7e9f7',
-        textDetails: '#9873b9',
-        primaryPurple: '#c9cbff',
-        secondaryPurple:'#553c8b',
-        tertiaryPurple :'#a08bcc'
-    },
-    fonts:{
-        titleWeight: '900',
-        fontWeight: '600',
-        fontFamily: "'Mosk', sans-serif",
-    },
-    mediaQueries: {
-        above600: '(min-width: 600px)',
-        above768: '(min-width: 768px)',
-        above1024: '(min-width: 1024px)',
-      },
-    paddings:{
-        sectionMobile: '2rem 3rem 0',
-        sectionDesktop:'7rem 4rem 0'
-    }
-}
+	colors: {
+		primaryText: '#404040',
+		primaryBackground: '#f3f1f5',
+		secondaryBackground: '#eff8ff',
+		gifosBackground: '#a08bcc',
+		marvelBackground: '#f58e8e',
+		rewardsBackground: '#9edfee',
+		reservaBackground: '#d7e9f7',
+		textDetails: '#9873b9',
+		primaryPurple: '#c9cbff',
+		secondaryPurple: '#553c8b',
+		tertiaryPurple: '#a08bcc',
+	},
+	fonts: {
+		titleWeight: '900',
+		fontWeight: '600',
+		fontFamily: "'Mosk', sans-serif",
+	},
+	mediaQueries: {
+		above600: '(min-width: 600px)',
+		above768: '(min-width: 768px)',
+		above1024: '(min-width: 1024px)',
+		above1200: '(min-width: 1200px)',
+	},
+	paddings: {
+		sectionMobile: '2rem 3rem 0',
+		sectionDesktop: '7rem 6rem 0',
+	},
+};
 
 export default theme;
