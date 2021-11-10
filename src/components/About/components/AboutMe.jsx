ec85a4149a211a646b5b741833e004d6db217f22
Mafer <jmmariafernanda15@gmail.com>
2021-11-10 17:39:21 -0300

    Creates About me section

---
+++ src/components/About/components/AboutMe.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import TabsContainer from '../../../styles/TabsContainer';
+import AboutMeContainer from './AboutMeStyles';
+import MaferPic from '../assets/mafer-pic.jpg';
+import DinoCloudLogo from '../assets/dinocloud-logo.png';
+import LaptopIcon from '../assets/laptop-icon.png';
+import PlantIcon from '../assets/plant-icon.png';
+import JoystickIcon from '../assets/joystick-icon.png';
+
+function AboutMe() {
+	return (
+		<TabsContainer>
+			<div className="tab-container">
+				<div className="tab-dots"></div>
+				<div className="tab-dots"></div>
+				<div className="tab-dots"></div>
+			</div>
+			<AboutMeContainer>
+				<div className="top-container">
+					<div className="about-me-img-wrapper">
+						<img src={MaferPic} alt="A pic of me :D!" />
+					</div>
+					<div className="about-me-info">
+						<div className="about-me-item">
+							<img
+								className="item-icon"
+								src={LaptopIcon}
+								alt="Icon of a laptop"
+							/>{' '}
+							<p>Working at </p>
+							<a href="https://dinocloudconsulting.com/" target="_blank">
+								<img
+									className="dinocloud-logo"
+									src={DinoCloudLogo}
+									alt="DinoCloud Logo"
+								/>
+							</a>
+						</div>
+						<div className="about-me-item">
+							<img
+								className="item-icon"
+								src={PlantIcon}
+								alt="Icon of a plant"
+							/>{' '}
+							<p>Currently learning Redux</p>
+						</div>
+						<div className="about-me-item">
+							<img
+								className="item-icon"
+								src={JoystickIcon}
+								alt="Icon of a joystick"
+							/>{' '}
+							<p>Currently playing Fez</p>
+						</div>
+						<div className="about-me-item">
+							<p>
+								<span>Fun Fact:</span> I love making playlist on spotify, I've
+								made more than 100!
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="bottom-container">
+					<h3>README</h3>
+					<p>
+						A very curious and simple person who enjoys coding, born and raised
+						in Valencia, Venezuela, currently based in Córdoba, Argentina.
+					</p>
+					<p>
+						I started my career as a developer in early 2020 thanks to a
+						scholarship granted by Globant, from that moment I realized how in
+						love I'm with the IT world (cheesy, I know). I'm most comfortable in
+						that weird, funky world between design and development.
+					</p>
+					<p>
+						Apart from coding, I like watching animated movies and TV series,
+						listening and discovering music, and outdoor activities such a
+						walking my dogs and jumping rope at the park.
+					</p>
+				</div>
+			</AboutMeContainer>
+		</TabsContainer>
+	);
+}
+
+export default AboutMe;
---
+++ src/components/About/components/AboutMeStyles.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import theme from "../../../styles/theme";
+
+
+const AboutMeContainer = styled.div`
+    padding: 1rem 1.5rem;
+    @media ${theme.mediaQueries.above1200}{
+        max-width: 43rem;
+    }
+    
+
+    @media ${theme.mediaQueries.above768} {
+        .top-container{
+            display: flex;
+        }
+    }
+
+    .about-me-info{
+        margin-bottom: 2rem;
+
+        @media ${theme.mediaQueries.above768}{
+            margin-left: 10px;
+        }
+    }
+
+    .about-me-img-wrapper img{
+        width: -webkit-fill-available;
+        max-width: 23rem;
+        margin-bottom: 1rem;
+        border-radius: 15px;
+    }
+
+    .about-me-item{
+        margin-bottom: 10px;
+        display: flex;
+        align-items: center;
+    }
+
+    .about-me-item .item-icon {
+        width: 1.5rem;
+        margin-right: 10px;
+    }
+
+    .dinocloud-logo{
+        margin-left: 5px;
+        height: 30px;
+    }
+
+    .bottom-container h3{
+        margin-bottom: .5rem;
+    }
+
+    .bottom-container p{
+        margin-bottom: 12px;
+    }
+`;
+
+export default AboutMeContainer;
