91fda70ec78dd62d851151080c4b2e2ddc3f858d
Mafer <jmmariafernanda15@gmail.com>
2021-11-12 14:56:33 -0300

    Adds background colors for hover on Project

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -4,9 +4,13 @@ const theme = {
 		primaryBackground: '#f3f1f5',
 		secondaryBackground: '#eff8ff',
 		gifosBackground: '#a08bcc',
-		marvelBackground: '#f58e8e',
+		marvelBackground: '#f58383',
 		rewardsBackground: '#9edfee',
 		reservaBackground: '#d7e9f7',
+		gifosBackgroundHover: 'rgb(160, 139, 204, 0.8)',
+		marvelBackgroundHover: 'rgb(245, 142, 142, 0.8)',
+		rewardsBackgroundHover: 'rgb(158, 223, 238, 0.8)',
+		reservaBackgroundHover: 'rgb(215, 233, 247, 0.8)',
 		textDetails: '#9873b9',
 		primaryPurple: '#c9cbff',
 		secondaryPurple: '#553c8b',
@@ -22,10 +26,11 @@ const theme = {
 		above768: '(min-width: 768px)',
 		above1024: '(min-width: 1024px)',
 		above1200: '(min-width: 1200px)',
+		above1366: '(min-width: 1366px)',
 	},
 	paddings: {
-		sectionMobile: '2rem 3rem 0',
-		sectionDesktop: '7rem 4rem 0',
+		sectionMobile: '7rem 3rem 0',
+		sectionDesktop: '12rem 4rem 0',
 	},
 };
 
