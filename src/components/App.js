f1c40e0df3b6a10cdef4df17158ba841c2792952
Mafer <jmmariafernanda15@gmail.com>
2021-11-12 14:59:20 -0300

    Adds Projects to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Hero from "./Hero/Hero"
 import About from "./About/About"
-
+import Projects from "./Projects/Projects";
 
 function App() {
   return (
    <div className="App">
      <Hero />
      <About />
+     <Projects />
    </div>
   );
 }
