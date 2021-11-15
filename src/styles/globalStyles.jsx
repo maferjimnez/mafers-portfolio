6fab387be9315a3b3d8dc6e5d5bd044b3015da94
Mafer <jmmariafernanda15@gmail.com>
2021-11-15 20:45:30 -0300

    Fixes font bug

--- src/styles/globalStyles.jsx
+++ src/styles/globalStyles.jsx
@@ -1,19 +1,34 @@
 import { createGlobalStyle } from 'styled-components';
 import theme from './theme';
+import MoskFont from './fonts/Mosk.woff';
+import MoskBoldFont from './fonts/Mosk-ultra-bold.woff';
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+font-family: 'Mosk Semi-Bold 600';
+font-style: normal;
+font-weight: normal;
+src: local('Mosk Semi-Bold 600'), url(${MoskFont}) format('woff');
+}
+
+@font-face {
+    font-family: 'Mosk Ultra-Bold 900';
+    font-style: normal;
+    font-weight: normal;
+    src: local('Mosk Ultra-Bold 900'), url(${MoskBoldFont}) format('woff');
+    }
 * {
 	margin: 0;
 	padding: 0;
 	box-sizing: border-box;
-	font-family: ${theme.fonts.fontFamily};
+	font-family: 'Mosk Semi-Bold 600';
 	font-weight: ${theme.fonts.fontWeight};
 	color: ${theme.colors.primaryText};
 	transition: 0.3s ease-in;
     }
 
 h1, h2, h3, h4, h5, h6 {
-	font-weight: ${theme.fonts.titleWeight};
+	font-family: 'Mosk Ultra-Bold 900';
 }
 
 h1{
