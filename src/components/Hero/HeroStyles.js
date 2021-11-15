40d2d92eb0d7e69a0048bc2f4460dd39419c4161
Mafer <jmmariafernanda15@gmail.com>
2021-11-15 14:34:37 -0300

    Adds animations on Hero

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,6 +1,26 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import theme from "../../styles/theme";
 
+const bounce = keyframes`
+    0% {transform: translateY(0);}
+    20% {transform: translateY(0);}
+    50% {transform: translateY(0);}
+    80% {transform: translateY(0);}
+    100% {transform: translateY(0);} 
+    40% {transform: translateY(-30px);} 
+    60% {transform: translateY(-15px);} 
+`;
+
+const spin = keyframes`
+    0%{transform: rotateY(120deg);}
+    15% {transform: rotateY(105deg);}
+    30% {transform: rotateY(90deg);}
+    45% {transform: rotateY(75deg);}
+    60% {transform: rotateY(60deg);}
+    75% {transform: rotateY(45deg);}
+    90% {transform: rotateY(30deg);}
+    100% {transform: rotateY(0);} 
+`;
 
 const StyledHero = styled.section`
     padding: ${theme.paddings.sectionMobile};
@@ -38,6 +58,7 @@ const StyledHero = styled.section`
         border-radius: 50%;
         border: 5px dashed ${theme.colors.textDetails};
         background-color: ${theme.colors.secondaryBackground};
+        animation: ${spin} 1s ease-in-out;
 
         @media ${theme.mediaQueries.above600} {
             max-width: 25rem;
@@ -46,6 +67,11 @@ const StyledHero = styled.section`
 
     .hero-img-wrapper img {
         width: 100%;
+    }
+
+    .exclamation-mark{
+        display: inline-block;
+        animation: ${bounce} 2s 1;
     }
 `;
 
